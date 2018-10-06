16cb92e46b2f27a14eb2a8e82f3917cae6650c06
League <League@AdminisorsiMac2.attlocal.net>
2018-10-06 11:55:36 -0700

    my first java program

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -1,14 +1,47 @@
 package section2;
 
 import java.awt.Color;
+import java.util.Random;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
 	
 	public static void main(String[] args) {
+	Random r = new Random();	
+		Robot markZuckerburg = new Robot();
+		markZuckerburg.setSpeed(100);
+		markZuckerburg.move(50);
+		markZuckerburg.turn(90);
+		markZuckerburg.move(50);
+		markZuckerburg.turn(90);
+		markZuckerburg.move(50);
+		markZuckerburg.turn(90);
+		markZuckerburg.move(50);
+		markZuckerburg.turn(90);
+		markZuckerburg.move(50);
+		markZuckerburg.turn(90);
+		markZuckerburg.move(50);
+		markZuckerburg.turn(90);
+		markZuckerburg.move(50);
+		markZuckerburg.turn(90);
+		markZuckerburg.move(50);
+		markZuckerburg.penDown();
+		markZuckerburg.setPenColor(r.nextInt(255), r.nextInt(255), r.nextInt(255));
+		markZuckerburg.move(50);
+		markZuckerburg.turn(90);
+		markZuckerburg.move(50);
+		markZuckerburg.turn(90);markZuckerburg.move(50);
+		markZuckerburg.turn(90);
+		markZuckerburg.move(50);
+		markZuckerburg.turn(90);markZuckerburg.move(50);
+		markZuckerburg.turn(90);
+		markZuckerburg.move(50);
+		markZuckerburg.turn(90);
+		markZuckerburg.penUp();
 		
-		// START HERE
-	
+		
+		
 		
 		
 	}
